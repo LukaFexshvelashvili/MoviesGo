a89bd7e5b7ee58ea31e70270f52a837b93d2ab21
Luka <battelerx@gmail.com>
2025-02-09 23:10:46 +0400

    Started moviesgo V2

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bodybg: "#1c1c1c",
+      },
+      fontFamily: {
+        mbold: "mbold",
+        mmedium: "mmedium",
+        mregular: "mregular",
+        msemibold: "msemibold",
+      },
+    },
+  },
+  plugins: [],
+};
